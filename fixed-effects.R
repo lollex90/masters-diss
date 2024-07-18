b1d85f8cb04b6ac3a6d657508d6902bbd396d566
lollex90 <j.m.terlikowski@lse.ac.uk>
2024-07-18 22:39:58 +0100

    minor changes

--- fixed-effects.R
+++ fixed-effects.R
@@ -6,6 +6,8 @@ sum_columns <- c("nearnad_snow_cov_sum", "nearnad_snow_free_sum", "offnad_snow_c
                         "offnad_snow_free_sum", "allangle_snow_cov_sum", "allangle_snow_free_sum", 
                         "nearnad_snow_free_hq_sum", "offnad_snow_free_hq_sum", "allangle_snow_free_hq_sum")
 
+sum_columns <- c("allangle_snow_free_hq_sum")
+
 # read in the data
 ukraine_data <- read.csv("data/tabular_data_ukraine.csv")
 
@@ -16,37 +18,58 @@ train_data <- ukraine_data %>%
 test_data <- ukraine_data %>%
   filter(year == 2021)
 
+test_data$year <- 2020  # Temporarily use the last year of the training data
+
 full_data <- ukraine_data %>%
   filter(year < 2022)
 
 prediction_data <- ukraine_data %>%
-  filter(year > 2021)
+  filter(year == 2022)
+
+prediction_data$year <- 2021  # Temporarily use the last year of the training data
 
 for (indep_var in sum_columns) {
   
-  formula <- as.formula(paste("log(real_gdp) ~ log(", indep_var, ") + region", sep = ""))
-  model <- lm(formula, data = train_data)
+  # train region-only model
+  formula_reg <- as.formula(paste("log(real_gdp) ~ log(", indep_var, ") + region", sep = ""))
+  model_reg<- lm(formula_reg, data = train_data)
   
-  # use the model to predict for the test data
-  test_data$real_gdp_pred <- exp(predict(model, test_data))
+  # train region and year model
+  formula_year <- as.formula(paste("log(real_gdp) ~ log(", indep_var, ") + region + factor(year)", sep = ""))
+  model_year <- lm(formula_year, data = train_data)
+  
+  # get the coefficient for factor(year)2020
+  coef_2020 <- coef(model_year)[names(coef(model_year)) == "factor(year)2020"]
+  
+  # use the models
+  test_data$real_gdp_pred_reg <- exp(predict(model_reg, test_data))
+  test_data$real_gdp_pred_year <- exp(predict(model_year, test_data) - coef_2020)
   
   # calculate the absolute % error
-  test_data$error <- abs(((test_data$real_gdp - test_data$real_gdp_pred) / test_data$real_gdp) * 100)
+  test_data$error_reg <- abs(((test_data$real_gdp - test_data$real_gdp_pred_reg) / test_data$real_gdp) * 100)
+  test_data$error_year <- abs(((test_data$real_gdp - test_data$real_gdp_pred_year) / test_data$real_gdp) * 100)
   
   # print the mean % error
-  print(paste("Mean % error for", indep_var, "is", mean(test_data$error, na.rm = TRUE)))
+  print(paste("Mean % error for REG and ", indep_var, "is", mean(test_data$error_reg, na.rm = TRUE)))
+  print(paste("Mean % error for YEAR and ", indep_var, "is", mean(test_data$error_year, na.rm = TRUE)))
   
-  # estimate the same model on the entire dataset
-  model <- lm(formula, data = full_data)
+  # estimate the same models on the entire dataset
+  model_reg <- lm(formula_reg, data = full_data)
+  model_year <- lm(formula_year, data = full_data)
   
+  # get the coefficient for factor(year)2021
+  coef_2021 <- coef(model_year)[names(coef(model_year)) == "factor(year)2021"]
+
   # predict on the prediction data
-  prediction_data$real_gdp_pred <- exp(predict(model, prediction_data))
-  
+  prediction_data$real_gdp_pred_reg <- exp(predict(model_reg, prediction_data))
+  prediction_data$real_gdp_pred_year <- exp(predict(model_year, prediction_data) - coef_2021)
+
   # calculate the gdp change from 2021 to 2022
-  prediction_data <- prediction_data %>% filter(year == 2022) 
-  gdp_change <- ((sum(prediction_data$real_gdp_pred) - sum(test_data$real_gdp))/sum(test_data$real_gdp)) * 100
-  
-  print(paste("GDP change for", indep_var, "is", gdp_change))
+  gdp_change_reg <- ((sum(prediction_data$real_gdp_pred_reg) - sum(test_data$real_gdp))/sum(test_data$real_gdp)) * 100
+  gdp_change_year <- ((sum(prediction_data$real_gdp_pred_year) - sum(test_data$real_gdp))/sum(test_data$real_gdp)) * 100
+
+  print(paste("GDP REG change for", indep_var, "is", gdp_change_reg))
+  print(paste("GDP YEAR change for", indep_var, "is", gdp_change_year))
 
 }
 
--- gdp_map.R
+++ gdp_map.R
@@ -1,4 +1,7 @@
 library(tidyverse)
+library(geojsonio)
+library(ggplot2)
+library(sf)
 
 # read gdp prediction
 gdp_2022 <- read.csv("data/gdp_predictions_ukraine.csv")
@@ -10,5 +13,26 @@ gdp_2021 <- read.csv("data/gdp_ukraine_clean.csv") %>%
 # merge by region, calculate difference
 gdp <- gdp_2022 %>% 
   left_join(gdp_2021, by = "region") %>% 
-  mutate(diff = 100*(gdp_prediction - real_gdp)/real_gdp)
+  mutate(diff = 100*(gdp_prediction - real_gdp)/real_gdp) %>% 
   select(region, real_gdp, gdp_prediction, diff)
+
+# read the geojson file
+ukraine_map <- geojson_read("geoBoundaries-UKR-ADM1.geojson", what = "sp")
+  
+# clean the region names: replace spaces with "_"
+ukraine_map@data$shapeName <- gsub(" ", "_", ukraine_map@data$shapeName)
+
+# merge the map with the gdp data
+ukraine_map@data <- left_join(ukraine_map@data, gdp, by = c("shapeName" = "region"))
+
+ukraine_map_sf <- st_as_sf(ukraine_map)
+
+# plot the map
+map <- ggplot(data = ukraine_map_sf) +
+  geom_sf(aes(fill = diff), color = "black") +
+  scale_fill_gradient2(low = "red", high = "white", midpoint = 0, na.value = "grey") +
+  theme_minimal() +
+  theme(legend.position = "bottom") +
+  labs(title = "GDP Growth Prediction in Ukraine by Region in 2022",
+       fill = "GDP Growth (%)")
+map
